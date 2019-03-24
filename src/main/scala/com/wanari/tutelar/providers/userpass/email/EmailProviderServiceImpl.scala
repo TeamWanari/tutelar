6f3d06d49191759c3e7e5b247df34c1b3f06a7fb
ksisu <ksisu@wanari.com>
2019-03-24 18:55:34 +0100

    refactor email provider token

--- src/main/scala/com/wanari/tutelar/providers/userpass/email/EmailProviderServiceImpl.scala
+++ src/main/scala/com/wanari/tutelar/providers/userpass/email/EmailProviderServiceImpl.scala
@@ -5,7 +5,8 @@ import com.wanari.tutelar.core.{AuthService, JwtService}
 import com.wanari.tutelar.core.AuthService.Token
 import com.wanari.tutelar.providers.userpass.basic.BasicProviderServiceImpl
 import com.wanari.tutelar.providers.userpass.email.EmailProviderService.EmailProviderConfig
-import spray.json.{JsObject, JsString}
+import com.wanari.tutelar.providers.userpass.email.EmailProviderServiceImpl.EmailToken
+import spray.json._
 
 class EmailProviderServiceImpl[F[_]: MonadError[?[_], Throwable]](
     implicit emailService: EmailService[F],
@@ -23,14 +24,14 @@ class EmailProviderServiceImpl[F[_]: MonadError[?[_], Throwable]](
 
   override def register(registerToken: String, password: String, data: Option[JsObject]): F[Token] = {
     for {
-      email <- decodeToken(registerToken, EmailProviderServiceImpl.RegisterToken)
+      email <- decodeToken(registerToken, EmailToken.RegisterType)
       token <- super.register(email, password, data)
     } yield token
   }
 
   def sendRegister(email: String): F[Unit] = {
     for {
-      token  <- createToken(email, EmailProviderServiceImpl.RegisterToken)
+      token  <- createToken(email, EmailToken.RegisterType)
       url    <- createRegistrationUrl(token)
       result <- emailService.sendRegisterUrl(email, url)
     } yield result
@@ -38,7 +39,7 @@ class EmailProviderServiceImpl[F[_]: MonadError[?[_], Throwable]](
 
   override def resetPassword(resetPasswordToken: String, password: String, data: Option[JsObject]): F[Token] = {
     for {
-      email <- decodeToken(resetPasswordToken, EmailProviderServiceImpl.ResetPasswordToken)
+      email <- decodeToken(resetPasswordToken, EmailToken.ResetPasswordType)
       token <- changePasswordAndLogin(email, password, data)
     } yield token
   }
@@ -46,28 +47,26 @@ class EmailProviderServiceImpl[F[_]: MonadError[?[_], Throwable]](
   def sendResetPassword(email: String): F[Unit] = {
     for {
       _      <- checkIsExists(email)
-      token  <- createToken(email, EmailProviderServiceImpl.ResetPasswordToken)
+      token  <- createToken(email, EmailToken.ResetPasswordType)
       url    <- createResetPasswordUrl(token)
       result <- emailService.sendResetPasswordUrl(email, url)
     } yield result
   }
 
   private def decodeToken(registerToken: String, `type`: String): F[String] = {
-    (for {
-      service   <- jwtService
-      tokenData <- service.decode(registerToken)
-    } yield {
-      for {
-        _               <- tokenData.fields.find(x => x._1 == "type" && x._2 == JsString(`type`))
-        JsString(email) <- tokenData.fields.get("email")
-      } yield email
-    }).flatMap(_.pureOrRaise(new Exception()))
+    import EmailProviderServiceImpl._
+    for {
+      service    <- jwtService
+      tokenData  <- service.decode(registerToken)
+      emailToken <- tokenData.convertToF[F, EmailToken]
+      _          <- (emailToken.`type` == `type`).pureUnitOrRise(new Exception)
+    } yield emailToken.email
   }
 
   private def createToken(email: String, `type`: String): F[String] = {
     for {
       service <- jwtService
-      token   <- service.encode(JsObject("email" -> JsString(email), "type" -> JsString(`type`)))
+      token   <- service.encode(EmailToken(email, `type`).toJson.asJsObject)
     } yield token
   }
 
@@ -92,6 +91,12 @@ class EmailProviderServiceImpl[F[_]: MonadError[?[_], Throwable]](
 }
 
 object EmailProviderServiceImpl {
-  val RegisterToken      = "register"
-  val ResetPasswordToken = "reset"
+  case class EmailToken(email: String, `type`: String)
+  object EmailToken {
+    val RegisterType      = "register"
+    val ResetPasswordType = "reset"
+  }
+
+  import DefaultJsonProtocol._
+  implicit val emailTokenFormat: RootJsonFormat[EmailToken] = jsonFormat2(EmailToken.apply)
 }
