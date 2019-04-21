29a08b64b4c8cc7b24f1b2d2576d1e86ebe03d7c
ksisu <ksisu@wanari.com>
2019-04-21 17:39:23 +0200

    small trace refactor

--- src/main/scala/com/wanari/tutelar/util/TracingDirectives.scala
+++ src/main/scala/com/wanari/tutelar/util/TracingDirectives.scala
@@ -3,7 +3,7 @@ package com.wanari.tutelar.util
 import akka.http.scaladsl.server.Directive1
 import akka.http.scaladsl.server.Directives.{extractRequest, mapResponse, provide}
 import io.opentracing.{Span, Tracer}
-import io.opentracing.propagation.Format
+import io.opentracing.propagation.{Format, TextMapAdapter}
 
 import scala.util.Try
 
@@ -11,11 +11,13 @@ import scala.util.Try
 trait TracingDirectives {
 
   def trace(tracer: Tracer, operationName: String): Directive1[Span] = extractRequest.flatMap { req =>
-    val parent = Try(
+    val parent = Try {
+      import scala.collection.JavaConverters._
+      val headers = req.headers.map(h => (h.name(), h.value())).toMap.asJava
       // This method will throw an IllegalArgumentException for a bad
       // tracer header, or return null for no header. Handle both cases as None
-      tracer.extract(Format.Builtin.HTTP_HEADERS, new AkkaHttpHeaderExtractor(req.headers))
-    ).filter(_ != null).toOption
+      tracer.extract(Format.Builtin.HTTP_HEADERS, new TextMapAdapter(headers))
+    }.filter(_ != null).toOption
     val span = parent.fold(tracer.buildSpan(operationName).start())(
       p => tracer.buildSpan(operationName).asChildOf(p).start()
     )
