99b2b90142a3e43ed29217a235d9f3bd06deafe4
Ksisu <horkris@gmail.com>
2019-09-29 19:24:22 +0200

    refactor healthcheck api

--- src/it/scala/com/wanari/tutelar/core/HealthCheckItSpec.scala
+++ src/it/scala/com/wanari/tutelar/core/HealthCheckItSpec.scala
@@ -1,28 +1,47 @@
 package com.wanari.tutelar.core
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
+import cats.data.EitherT
+import com.wanari.tutelar.RouteTestBase
+import com.wanari.tutelar.core.Errors.{AppError, ErrorOr}
 import com.wanari.tutelar.core.HealthCheckService._
-import com.wanari.tutelar.{BuildInfo, RouteTestBase}
+
+import scala.concurrent.Future
 
 class HealthCheckItSpec extends RouteTestBase {
-  import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+
+  trait TestScope {
+    def serviceResult: HealthCheckResult
+    implicit lazy val service: HealthCheckService[Future] = new HealthCheckService[Future] {
+      import cats.instances.future._
+      override def getStatus: ErrorOr[Future, HealthCheckResult] = EitherT.pure[Future, AppError](serviceResult)
+    }
+    lazy val route = new HealthCheckApi().route
+  }
+
+  val basicResult = HealthCheckResult(
+    success = true,
+    "version",
+    database = true,
+    "time",
+    1111L,
+    Some("hash")
+  )
 
   "GET /healthCheck" should {
-    "return OK" in new BaseTestScope {
+    "return OK" in new TestScope {
+      val serviceResult = basicResult
       Get("/healthCheck") ~> route ~> check {
         status shouldEqual StatusCodes.OK
+        responseAs[HealthCheckResult] shouldEqual serviceResult
       }
     }
-    "return with data" in new BaseTestScope {
+    "return 500 when not success" in new TestScope {
+      val serviceResult = basicResult.copy(success = false)
       Get("/healthCheck") ~> route ~> check {
-        responseAs[HealthCheckResult] shouldEqual HealthCheckResult(
-          true,
-          BuildInfo.version,
-          true,
-          BuildInfo.builtAtString,
-          BuildInfo.builtAtMillis,
-          BuildInfo.commitHash
-        )
+        status shouldEqual StatusCodes.InternalServerError
+        responseAs[HealthCheckResult] shouldEqual serviceResult
       }
     }
   }
--- src/main/scala/com/wanari/tutelar/core/HealthCheckApi.scala
+++ src/main/scala/com/wanari/tutelar/core/HealthCheckApi.scala
@@ -1,18 +1,26 @@
 package com.wanari.tutelar.core
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.wanari.tutelar.Api
+import com.wanari.tutelar.core.HealthCheckService._
 
 import scala.concurrent.Future
+import scala.util.Success
 
 class HealthCheckApi(implicit service: HealthCheckService[Future]) extends Api {
 
-  def route(): Route = {
+  def route: Route = {
     path("healthCheck") {
       get {
-        withTrace("HealthCheck") { implicit ctx =>
-          service.getStatus.toComplete
+        withTrace("HealthCheck") { _ =>
+          onComplete(service.getStatus.value) {
+            case Success(Right(res)) if res.success => complete(res)
+            case Success(Right(res))                => complete((StatusCodes.InternalServerError, res))
+            case _                                  => complete(StatusCodes.InternalServerError)
+          }
         }
       }
     }
